31779493e4acd91aab49dc66ba9a4a002a40cc10
JTokos <jqtok0829@gmail.com>
2020-05-08 13:02:06 -0700

    Added GameState

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-fn main() {
-    println!("Hello, world!");
-    println!("Got it!");
+use tetra::graphics::{self, Color};
+use tetra::{Context, ContextBuilder,State};
+fn main() -> tetra::Result{
+    ContextBuilder::new("Pog", 640, 480) //holds framework of game
+        .quit_on_escape(true) //esc key to close game
+        .build()?
+        .run(|_ctx| Ok(GameState{}))
 }
+
+struct GameState{}
+//stores current state of the game
+impl State for GameState{}
