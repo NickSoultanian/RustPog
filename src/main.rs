e8eaab0d94762940e7f57b63515987f379659203
Nicholas Soultanian <nicholas.soultanian@gmail.com>
2020-05-08 13:14:40 -0700

    Player1 added!

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,62 @@
-use tetra::graphics::{self, Color};
-use tetra::{Context, ContextBuilder,State};
+use tetra::graphics::{self, Color, Texture};
+use tetra::input::{self, Key};
+use tetra::math::Vec2;
+use tetra::{Context, ContextBuilder, State};
+
+const PADDLE_SPEED: f32 = 8.0;
+const WINDOW_WIDTH: f32 = 640.0;
+const WINDOW_HEIGHT: f32 = 480.0;
+
 fn main() -> tetra::Result{
-    ContextBuilder::new("Pog", 640, 480) //holds framework of game
+    ContextBuilder::new("Pog", WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32) //holds framework of game
         .quit_on_escape(true) //esc key to close game
         .build()?
-        .run(|_ctx| Ok(GameState{}))
+        .run(GameState::new)
 }
+struct Entity {
+    texture: Texture,
+    position: Vec2<f32>,
+}
+impl Entity {
+    fn new(texture: Texture, position: Vec2<f32>) -> Entity {
+        Entity { texture, position }
+    }
+}
+struct GameState {
+    player1: Entity,
+}
+impl GameState {
+    fn new(ctx: &mut Context) -> tetra::Result<GameState> {
+        let player1_texture = Texture::new(ctx, "./resources/player1.png")?;
+        let player1_position = Vec2::new(
+            16.0,
+            (WINDOW_HEIGHT - player1_texture.height() as f32) / 2.0,
+        );
 
-struct GameState{}
-
+        Ok(GameState {
+            player1: Entity::new(player1_texture, player1_position),
+        })
+    }
+}
 //stores current state of the game
-impl State for GameState{
+impl State for GameState {
+    fn update(&mut self, ctx: &mut Context) -> tetra::Result {
+        if input::is_key_down(ctx, Key::W) {
+            self.player1.position.y -= PADDLE_SPEED;
+        }
+
+        if input::is_key_down(ctx, Key::S) {
+            self.player1.position.y += PADDLE_SPEED;
+        }
+
+        Ok(())
+    }
+
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
-        graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));//background color
+        graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+
+        graphics::draw(ctx, &self.player1.texture, self.player1.position);
+
 
         Ok(())
     }
